2e0fb8db7ddb457e791e0ca0699e165103356df7
Manuel Rivera <leunknownr@gmail.com>
2025-07-05 10:33:11 -0500

    Cambiando formato de método sign

--- src/nodes/signed-info.js
+++ src/nodes/signed-info.js
@@ -2,37 +2,55 @@ import xmlbuilder2 from "xmlbuilder2";
 import crypto from "crypto";
 import fs from "fs";
 import { canonicalizeXml } from "../canonicalize.js";
-import { CANONICALIZATION_METHOD_STANDARD_URL, SHA_256_STANDARD_URL, XADES_STANDARD_URL, XML_SIGNATURE_STANDARD_ENVELOPED_SIGNATURE_URL, XML_SIGNATURE_STANDARD_URL } from "../constants.js";
+import {
+	CANONICALIZATION_METHOD_STANDARD_URL,
+	SHA_256_STANDARD_URL,
+	XADES_STANDARD_URL,
+	XML_SIGNATURE_STANDARD_ENVELOPED_SIGNATURE_URL,
+	XML_SIGNATURE_STANDARD_URL,
+} from "../constants.js";
 
 export function buildXmlDigest() {
 	const xml = fs.readFileSync("invoice.xml", "utf8");
-	const xmlObject = xmlbuilder2.convert(xml, { format: 'object' });
-	fs.writeFileSync("tmp-invoice.xml", xmlbuilder2.create(xmlObject).end({ prettyPrint: false }));
+	const xmlObject = xmlbuilder2.convert(xml, { format: "object" });
+	fs.writeFileSync(
+		"tmp-invoice.xml",
+		xmlbuilder2.create(xmlObject).end({ prettyPrint: false })
+	);
 	const canonicalizedXml = canonicalizeXml("tmp-invoice.xml");
 	fs.unlinkSync("tmp-invoice.xml");
-	return crypto.createHash('sha256').update(canonicalizedXml).digest("base64");
+	return crypto.createHash("sha256").update(canonicalizedXml).digest("hex");
 }
-export function buildSignedPropertiesDigest({ signedPropertiesObj, companyCertificate }) {
-	const signedPropertiesXML = xmlbuilder2.create({
-		"xades:SignedProperties": {
-			...signedPropertiesObj["xades:SignedProperties"],
-			"@xmlns:ds": XML_SIGNATURE_STANDARD_URL,
-			"@xmlns:xades": XADES_STANDARD_URL,
-		},
-	}).end({
-		prettyPrint: false,
-	});
+export function buildSignedPropertiesDigest({
+	signedPropertiesObj,
+	companyCertificate,
+}) {
+	const signedPropertiesXML = xmlbuilder2
+		.create({
+			"xades:SignedProperties": {
+				...signedPropertiesObj["xades:SignedProperties"],
+				"@xmlns:ds": XML_SIGNATURE_STANDARD_URL,
+				"@xmlns:xades": XADES_STANDARD_URL,
+			},
+		})
+		.end({
+			prettyPrint: false,
+		});
 	fs.writeFileSync("signed-properties.xml", signedPropertiesXML);
 	const canonicalizedXml = canonicalizeXml("signed-properties.xml");
 	fs.unlinkSync("signed-properties.xml");
-	const signer = crypto.createSign('sha256');
-	signer.update(canonicalizedXml);
-	signer.end();
-	const digest = signer.sign(companyCertificate.privateKey, "base64");
-	return digest;
+	return crypto
+		.createSign("sha256")
+		.update(canonicalizedXml)
+		.end()
+		.sign(companyCertificate.privateKey, "base64");
 }
 
-export function buildSignedInfoNode({ signatureId, signedPropertiesObj, companyCertificate }) {
+export function buildSignedInfoNode({
+	signatureId,
+	signedPropertiesObj,
+	companyCertificate,
+}) {
 	return {
 		"ds:SignedInfo": {
 			"ds:CanonicalizationMethod": {
@@ -48,7 +66,8 @@ export function buildSignedInfoNode({ signatureId, signedPropertiesObj, companyC
 					"@Id": `${signatureId}-ref`,
 					"ds:Transforms": {
 						"ds:Transform": {
-							"@Algorithm": XML_SIGNATURE_STANDARD_ENVELOPED_SIGNATURE_URL,
+							"@Algorithm":
+								XML_SIGNATURE_STANDARD_ENVELOPED_SIGNATURE_URL,
 						},
 					},
 					"ds:DigestMethod": {
@@ -62,8 +81,10 @@ export function buildSignedInfoNode({ signatureId, signedPropertiesObj, companyC
 					"ds:DigestMethod": {
 						"@Algorithm": SHA_256_STANDARD_URL,
 					},
-					"ds:DigestValue":
-						buildSignedPropertiesDigest({ signedPropertiesObj, companyCertificate }),
+					"ds:DigestValue": buildSignedPropertiesDigest({
+						signedPropertiesObj,
+						companyCertificate,
+					}),
 				},
 			],
 		},
